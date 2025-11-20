16df7a82eaab193fff615b0e46a90a8a759558a5
ngductri <trindse184856@fpt.edu.vn>
2025-11-20 13:08:53 +0700

    remove unused folders

--- Think2Drive/carla_agent/reward_criteria/__init__.py
+++
@@ -1,6 +0,0 @@
-from .timeout import Timeout
-from .route_deviation import RouteDeviation
-from .route_completion import RouteCompletion
-from .collision import Collision
-from .run_red_light import RunRedLight
-from .run_stop_sign import RunStopSign
--- Think2Drive/dreamerv3/embodied/run/__init__.py
+++
@@ -1,9 +0,0 @@
-from .eval_only import eval_only
-from .train import train
-from .eval_only_carla_parallel import eval_only_carla_parallel
-from .train_eval import train_eval
-from .train_holdout import train_holdout
-
-from . import parallel
-from . import parallel_carla
-from . import parallel_with_eval
